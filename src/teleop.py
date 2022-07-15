33871788472b864edb1df3bb17daa4d31ba323b4
Albert Johannessen <aljoh@equinor.com>
2022-07-15 16:01:17 +0200

    Rework arm

--- src/teleop.py
+++ src/teleop.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 
 import math
+import numpy as np
 
 from pynput import keyboard
 
 from collections import defaultdict
 
 import rospy
-from std_msgs.msg import Header, Bool
+from std_msgs.msg import Header, Bool, Float64MultiArray
 from geometry_msgs.msg import Twist, Point
 from sensor_msgs.msg import JointState, Joy
 from moveit_msgs.msg import MotionPlanRequest, Constraints, JointConstraint, PositionIKRequest, RobotState
 from moveit_msgs.srv import GetPositionIK, GetMotionPlan, GetPositionFK
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import FollowJointTrajectoryActionGoal, JointTolerance
+from actionlib_msgs.msg import GoalID
+from moveit_commander.move_group import MoveGroupCommander
 
 
 keybindings_arm = {
@@ -213,6 +216,10 @@ def main():
         time_previous = rospy.Time.now()
 
         arm_publisher = rospy.Publisher("arm_controller/follow_joint_trajectory/goal", FollowJointTrajectoryActionGoal, queue_size=10)
+        arm_cancel_publisher = rospy.Publisher("/arm_controller/follow_joint_trajectory/cancel", GoalID, queue_size=10)
+        #stop_publisher = rospy.Publisher("joint_trajectory_point", Float64MultiArray, queue_size=10)
+        #arm_cancel_publisher = rospy.Publisher("/execute_trajectory/cancel", GoalID, queue_size=10)
+        #arm_cancel_publisher = rospy.Publisher("/move_group/cancel", GoalID, queue_size=10)
         gripper_publisher = rospy.Publisher("gripper_controller/follow_joint_trajectory/goal", FollowJointTrajectoryActionGoal, queue_size=10)
         wheel_publisher = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
@@ -228,6 +235,8 @@ def main():
         # Necessary to compute the reference_position
         while not joint_state_subscriber.joint_state.position and not rospy.is_shutdown():
             pass
+        
+        joint_state_subscriber.joint_position_estimate = list(joint_state_subscriber.joint_state.position[2:6])
 
         print("Read initial joint state")
 
@@ -235,10 +244,10 @@ def main():
         arm_speed_x = 0.2
         arm_speed_y = 1
         arm_speed_z = 0.2
-        arm_motion_plan_duration = 0.001
+        arm_motion_plan_duration = 1
         arm_trajectory_points = 10
 
-        gripper_speed = 0.1
+        gripper_speed = 0.01
         gripper_motion_plan_duration = 0.1
         gripper_trajectory_points = 100
 
@@ -268,6 +277,11 @@ def main():
 
         reference_position = compute_fk(joint_state_subscriber.joint_state)
         reference_position.y = 0
+
+        reference_position_backup = Point()
+        reference_position_backup.x = 0
+        reference_position_backup.y = 0
+        reference_position_backup.z = 0
         
         reference_angle = joint_state_subscriber.joint_state.position[2]
 
@@ -281,7 +295,7 @@ def main():
         print("Computed initial position")
 
         print("Ready")
-        
+
         listener = keyboard.Listener(on_press=on_press, on_release=on_release)
         listener.start()
 
@@ -313,6 +327,10 @@ def main():
         goal_gripper.goal.trajectory = trajectory_gripper
 
         rate = rospy.Rate(100) # 100hz
+
+        active_keys_prev = active_keys
+
+        previous_goal_id = GoalID()
         
         while not rospy.is_shutdown():
 
@@ -324,86 +342,46 @@ def main():
             #if joy_subscriber.buttons[7] == 1:
             #    teleop_subscriber.stop()
             #    continue
-        
-            time_now = rospy.Time.now()
-            delta_time = (time_now - time_previous).to_sec()
-
-            publish_arm = False
 
             wheels_twist.linear.x = 0
             wheels_twist.angular.z = 0
-
-            reference_velocity.x = 0
-            reference_velocity.y = 0
-            reference_velocity.z = 0
-
-            reset_position = False
             
-            for key in list(active_keys):
-                if not active_keys[key]:
-                    del active_keys[key]
-                    continue
-                if key == "h":
-                    reset_position = True
-                    continue
-                if key in keybindings_arm:
-                    vx, vy, vz = keybindings_arm[key]
-                    reference_velocity.x += vx * arm_speed_x * delta_time
-                    reference_velocity.y += vy * arm_speed_y * delta_time
-                    reference_velocity.z += vz * arm_speed_z * delta_time
-                
-                elif key in keybindings_wheels:
-                    linear, angular = keybindings_wheels[key]
+            if active_keys["w"] and not active_keys["s"]:
+                wheels_twist.linear.x = wheels_linear_speed
+            elif active_keys["s"] and not active_keys["w"]:
+                wheels_twist.linear.x = - wheels_linear_speed
+            elif joy_subscriber.axes[1] != 0:
+                wheels_twist.linear.x = joy_subscriber.axes[1] * wheels_linear_speed
 
-                    wheels_twist.linear.x += linear * wheels_linear_speed * delta_time
-                    wheels_twist.angular.z += angular * wheels_angular_speed * delta_time
-            
-            if not reset_position and joy_subscriber.buttons[0] == 1:
-                reset_position = True
-            
-            if reset_position:
-                start_state = JointState()
-                start_state.header = joint_state_subscriber.joint_state.header
-                start_state.name = joint_state_subscriber.joint_state.name[2:6]
-                start_state.position = joint_state_subscriber.joint_state.position[2:6]
-                start_state.velocity = joint_state_subscriber.joint_state.velocity[2:6]
-                #start_state.effort = joint_state_subscriber.joint_state.effort[2:6]
-
-                end_state = JointState()
-                end_state.header = joint_state_subscriber.joint_state.header
-                end_state.name = joint_state_subscriber.joint_state.name[2:6]
-                end_state.position = [0, 0, 0, 0]
-                end_state.velocity = [0, 0, 0, 0]
-                #end_state.effort = [0, 0, 0, 0]
-
-                trajectory_home = plan_kinematic_path(start_state, end_state)
-
-                goal_arm.header.stamp = time_now
-                goal_arm.goal_id.stamp = time_now
-                goal_arm.goal_id.id = f"turtlebot_teleoperation_arm-{goal_arm.goal_id.stamp.secs}.{goal_arm.goal_id.stamp.nsecs}"
-                goal_arm.goal.trajectory = trajectory_home
-
-                arm_publisher.publish(goal_arm)
-
-                reference_position = compute_fk(end_state)
-                reference_position.y = 0
-                reference_angle = 0
-                continue
-            
-            wheels_twist.linear.x += joy_subscriber.axes[1] * wheels_linear_speed * delta_time
-            wheels_twist.angular.z += joy_subscriber.axes[0] * wheels_angular_speed * delta_time
+            if active_keys["a"] and not active_keys["d"]:
+                wheels_twist.angular.z += wheels_angular_speed
+            elif active_keys["d"] and not active_keys["a"]:
+                wheels_twist.angular.z += - wheels_angular_speed
+            elif joy_subscriber.axes[0] != 0:
+                wheels_twist.angular.z = joy_subscriber.axes[0] * wheels_angular_speed
 
             wheel_publisher.publish(wheels_twist)
-            
-            if not publish_arm and (joy_subscriber.axes[4] != 0 or joy_subscriber.axes[3] != 0 or joy_subscriber.axes[2] - joy_subscriber.axes[5] != 0):
-                reference_velocity.x += joy_subscriber.axes[4] * arm_speed_x * delta_time
-                reference_velocity.y += joy_subscriber.axes[3] * arm_speed_y * delta_time
-                reference_velocity.z += (joy_subscriber.axes[2] - joy_subscriber.axes[5]) / 2 * arm_speed_z * delta_time
-            
-            try:
-                if reference_velocity.x != 0 or reference_velocity.z != 0:
-                    reference_position.x += reference_velocity.x
-                    reference_position.z += reference_velocity.z
+
+            if active_keys["l"] or active_keys["j"] and not active_keys_prev["j"] or active_keys["i"] and not active_keys_prev["i"] or active_keys["k"] and not active_keys_prev["k"] or active_keys["o"] and not active_keys_prev["o"] or active_keys["u"] and not active_keys_prev["u"]:
+                try:
+                    if active_keys["l"] and not active_keys["j"] and reference_angle > -3:
+                        reference_angle -= 0.1
+                    elif active_keys["j"] and not active_keys["l"] and reference_angle < 3:
+                        reference_angle += 0.1
+
+                    reference_position_backup.x = reference_position.x
+                    reference_position_backup.z = reference_position.z
+
+                    if active_keys["i"] and not active_keys["k"]:
+                        reference_position.x += 0.01
+                    elif active_keys["k"] and not active_keys["i"]:
+                        reference_position.x -= 0.01
+                    
+                    if active_keys["o"] and not active_keys["u"]:
+                        reference_position.z += 0.01
+                    elif active_keys["u"] and not active_keys["o"]:
+                        reference_position.z -= 0.01
+                    
                     joint_state_ik = compute_ik(joint_state_subscriber.joint_state, reference_position)
 
                     # No solution
@@ -414,45 +392,76 @@ def main():
                         reference_position.z -= reference_velocity.z
                         raise IndexError
 
-                if reference_velocity.y != 0:
-                    if reference_angle < 0.8 * math.pi and reference_angle > - 0.8 * math.pi:
-                        reference_angle += reference_velocity.y
-                
-                if reference_velocity.x != 0 or reference_velocity.z != 0 or reference_velocity.y != 0:
                     trajectory_arm.points[0].positions = joint_state_subscriber.joint_state.position[2:6]
                     trajectory_arm.points[0].velocities = joint_state_subscriber.joint_state.velocity[2:6]
 
                     end_positions = [reference_angle] + list(joint_state_ik.position[1:4])
 
+                    arm_time = 0.1
+                    arm_motion_plan_duration = abs(reference_angle - joint_state_subscriber.joint_state.position[2]) / arm_trajectory_points * arm_time
+
                     for i in range(1, arm_trajectory_points + 1):
+                        #trajectory_arm.points[i].positions = end_positions
                         trajectory_arm.points[i].positions = [0, 0, 0, 0]
                         for j in range(4):
                             trajectory_arm.points[i].positions[j] = end_positions[j] * (i - 1) / (arm_trajectory_points - 1) + joint_state_subscriber.joint_state.position[j + 2] * (arm_trajectory_points - i) / (arm_trajectory_points - 1)
-                        trajectory_arm.points[i].velocities = [reference_velocity.y] + list(joint_state_ik.velocity[1:4])
+                        trajectory_arm.points[i].velocities = [0, 0, 0, 0]
+                        trajectory_arm.points[i].velocities[0] = -i * (i - arm_trajectory_points)
+                        trajectory_arm.points[i].time_from_start = rospy.Duration(arm_motion_plan_duration * i)
                     
                     goal_arm.goal.trajectory = trajectory_arm
-                    goal_arm.header.stamp = time_now
-                    goal_arm.goal_id.stamp = time_now
+                    goal_arm.header.stamp = rospy.Time.now()
+                    goal_arm.goal_id.stamp = rospy.Time.now()
                     goal_arm.goal_id.id = f"turtlebot_teleoperation_arm-{goal_arm.goal_id.stamp.secs}.{goal_arm.goal_id.stamp.nsecs}"
 
                     arm_publisher.publish(goal_arm)
-                        
-            except IndexError:
-                pass
+                
+                except IndexError:
+                    reference_position = reference_position_backup
             
-            action = None
-            gripper_key0, gripper_key1 = keybindings_gripper.keys()
-            if active_keys[gripper_key0] and not active_keys[gripper_key1] or joy_subscriber.buttons[4] == 0 and joy_subscriber.buttons[5] == 1:
-                action = keybindings_gripper[gripper_key0]
-            elif not active_keys[gripper_key0] and active_keys[gripper_key1] or joy_subscriber.buttons[4] == 1 and joy_subscriber.buttons[5] == 0:
-                action = keybindings_gripper[gripper_key1]
+            elif active_keys["h"] and not active_keys_prev["h"]:
+                start_state = JointState()
+                start_state.header = joint_state_subscriber.joint_state.header
+                start_state.name = joint_state_subscriber.joint_state.name[2:6]
+                start_state.position = joint_state_subscriber.joint_state.position[2:6]
+                start_state.velocity = joint_state_subscriber.joint_state.velocity[2:6]
 
-            if action is not None:
+                end_state = JointState()
+                end_state.header = joint_state_subscriber.joint_state.header
+                end_state.name = joint_state_subscriber.joint_state.name[2:6]
+                end_state.position = [0, 0, 0, 0]
+                end_state.velocity = [0, 0, 0, 0]
+
+                trajectory_home = plan_kinematic_path(start_state, end_state)
+
+                goal_arm.header.stamp = rospy.Time.now()
+                goal_arm.goal_id.stamp = rospy.Time.now()
+                goal_arm.goal_id.id = f"turtlebot_teleoperation_arm-{goal_arm.goal_id.stamp.secs}.{goal_arm.goal_id.stamp.nsecs}"
+                goal_arm.goal.trajectory = trajectory_home
+
+                arm_publisher.publish(goal_arm)
+
+                reference_position = compute_fk(end_state)
+                reference_position.y = 0
+                reference_angle = 0
+
+            gripper_action = None
+            
+            if active_keys["."] and not active_keys[","] or joy_subscriber.buttons[4] == 1 and joy_subscriber.buttons[5] == 0:
+                gripper_action = 1
+            elif active_keys[","] and not active_keys["."] or joy_subscriber.buttons[5] == 1 and joy_subscriber.buttons[4] == 0:
+                gripper_action = - 1
+            
+            if gripper_action:
                 try:
-                    goal_gripper.goal.trajectory.points[0].positions = [joint_state_subscriber.joint_state.position[0]]
-                    goal_gripper.goal.trajectory.points[0].velocities = [joint_state_subscriber.joint_state.velocity[0]]
+                    goal_gripper.goal.trajectory.points[0].positions = [joint_state_subscriber.joint_state.position[6]]
+                    goal_gripper.goal.trajectory.points[0].velocities = [joint_state_subscriber.joint_state.velocity[6]]
 
-                    goal_gripper.goal.trajectory.points[1].positions = [joint_state_subscriber.joint_state.position[0] + action * gripper_speed * delta_time]
+                    gripper_end = joint_state_subscriber.joint_state.position[6] + gripper_action * gripper_speed
+                    if gripper_end < 0.024:
+                        gripper_end = 0.024
+
+                    goal_gripper.goal.trajectory.points[1].positions = [gripper_end]
 
                     goal_gripper.header.stamp = rospy.Time.now()
                     goal_gripper.goal_id.stamp = rospy.Time.now()
@@ -462,8 +471,8 @@ def main():
                 
                 except IndexError:
                     pass
-
-            time_previous = time_now
+            
+            active_keys_prev = active_keys.copy()
         
     except rospy.ROSInterruptException:
         pass
